1df06e453bbcffe1f7889ee2b779f8063642d5d0
Niraj Kumar <nirajgupta7481@gmail.com>
2025-01-07 10:52:54 +0530

    subtract feature

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,9 @@
 import java.util.*;
 class Calculator{
 
+public static int subtract(int a, int b){
+return a - b;
+}		
 
 public static void main(String[] args){
 Scanner sc = new Scanner(System.in);
