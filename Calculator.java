8ebd97d5923a72e297334e8da788adf938c2e505
Niraj Kumar <nirajgupta7481@gmail.com>
2025-01-07 10:51:16 +0530

    divide feature

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,9 @@
 import java.util.*;
 class Calculator{
 
+public static double divide(int a, int b){
+return a / b;
+}
 
 public static void main(String[] args){
 Scanner sc = new Scanner(System.in);
